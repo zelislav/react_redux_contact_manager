1189724bc886a17962ec485ed9d837c3e7d25def
Zelislav Beska <zeljobeska@gmail.com>
2019-09-19 12:29:22 +0200

    Get request dispatch

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,11 +1,18 @@
 // this is the place where calls to the API will be done
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "./types";
+import axios from "axios";
 
-export const getContacts = () => {
-  return {
-    // return to the "reducers", getting initial content
-    type: GET_CONTACTS
-  };
+// "redux-thunk" allow us to call directly "dispatch"
+export const getContacts = () => async dispatch => {
+  // Making request to "jsonplaceholder"
+  // We want to get the response and then to add that as a payload to "GET_CONTACTS"
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+  // instead of calling "return" we wanna wrap it in "dispatch"
+  dispatch({
+    type: GET_CONTACTS,
+    // we can access to response data with "res.data"
+    payload: res.data
+  });
 };
 
 // "id" needed because it needs to know which one should be deleted
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -3,26 +3,7 @@ import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "../actions/types";
 // Each "reducer" has its own "initialState" and it's just going to export a default function
 const initialState = {
   // From "Contacts" component state moved to Redux
-  contacts: [
-    {
-      id: 1,
-      name: "User 1",
-      email: "user1@gmail.com",
-      phone: "111-111-1111"
-    },
-    {
-      id: 2,
-      name: "User 2",
-      email: "user2@gmail.com",
-      phone: "222-222-2222"
-    },
-    {
-      id: 3,
-      name: "User 3",
-      email: "user3@gmail.com",
-      phone: "333-333-3333"
-    }
-  ]
+  contacts: []
 };
 
 export default function(state = initialState, action) {
@@ -32,7 +13,9 @@ export default function(state = initialState, action) {
     case GET_CONTACTS:
       return {
         // initial state
-        ...state
+        ...state,
+        // to the initial state it sends along the "payload" which has all the stuff from "jsonplaceholder"
+        contacts: action.payload
       };
     case DELETE_CONTACT:
       return {
